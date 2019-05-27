8ea70fd4ea2bd10f7a63a86c5a4d3587fad231c2
Ziqian SUN (Zamir) <zsun@redhat.com>
2019-05-27 15:27:17 +0800

    sanity: Add xfsslower.sh

---
+++ sanity/xfsslower.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+bccprefix=$(rpm -ql bcc-tools | grep tools | head -n 1)
+BCCPATH=${bccpath:-${bccprefix}}
+LOGPREFIX=$(dirname $0)/logs/$(basename $0)
+
+. $(dirname $0)/../lib/createfs
+
+oneTimeSetUp(){
+rm -rf ${LOGPREFIX}
+mkdir -p ${LOGPREFIX}
+ps -ef | grep '/usr/share/bcc' | grep -v grep | awk '{print $2}' | xargs /usr/bin/kill -9 {} &> /dev/null || true
+}
+
+test_xfsslower_h(){
+    logfile=${LOGPREFIX}/${FUNCNAME[ 0 ]}.log
+    ${BCCPATH}/xfsslower -h &> ${logfile}
+    assertFileContains ${logfile} usage
+}
+
+test_xfsslower(){
+    createfsslow xfs 500
+    logfile=${LOGPREFIX}/${FUNCNAME[ 0 ]}.log
+    df -hT > ${logfile}
+    exec ${BCCPATH}/xfsslower &>> ${logfile} &
+    tpid=$!
+    sleep 5
+    dd if=/dev/zero of=${FSSLOWPATH}/${FUNCNAME[ 0 ]}.test count=500000
+    umount ${FSSLOWPATH}
+    sleep 3
+    /usr/bin/kill -INT $tpid
+    sleep 3
+    assertFileContains ${logfile} dd
+    destroyfsslow
+}
+
+test_xfsslower_num(){
+    createfsslow xfs 100
+    logfile=${LOGPREFIX}/${FUNCNAME[ 0 ]}.log
+    df -hT > ${logfile}
+    exec ${BCCPATH}/xfsslower 500 &>> ${logfile} &
+    tpid=$!
+    sleep 5
+    cp /boot/vmlinuz-$(uname -r) ${FSSLOWPATH}
+    dd if=/dev/zero of=${FSSLOWPATH}/${FUNCNAME[ 0 ]}.test count=500000
+    umount ${FSSLOWPATH}
+    sleep 3
+    /usr/bin/kill -INT $tpid
+    sleep 3
+    assertFileNotContains ${logfile} dd
+    destroyfsslow
+}
+
+. $(dirname $0)/../lib/include
+
+# Tests not covered
+# # ./xfsslower -j
+
